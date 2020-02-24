0bab5e8d61dcd5b0e481dd81985fe2b741e1fae3
sydneymainga6 <sydneymainga6@gmail.com>
2020-02-24 20:40:28 +0300

    room db registering functionality

--- app/src/main/java/com/example/sampleapp/ForgetPassword.java
+++ app/src/main/java/com/example/sampleapp/ForgetPassword.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.sampleapp.R;
+
 public class ForgetPassword extends AppCompatActivity {
     EditText email_editText;
     Button forgetpassword;
--- app/src/main/java/com/example/sampleapp/Selection.java
+++ app/src/main/java/com/example/sampleapp/Selection.java
@@ -3,13 +3,22 @@ package com.example.sampleapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.sampleapp.db.AppDatabase;
+import com.example.sampleapp.db.PersonInfo;
+import com.example.sampleapp.db.PersonInfoDao;
+
 import java.util.List;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
@@ -18,7 +27,7 @@ import butterknife.OnClick;
 public class Selection extends AppCompatActivity{
 
 
-    EditText fname,lname,reg_email,reg_password;
+    private EditText f_name,l_name,reg_email,reg_password;
     Button reg,signup ;
 
    @Override
@@ -27,6 +36,10 @@ public class Selection extends AppCompatActivity{
             setContentView(R.layout.activity_selection);
 
        signup =findViewById(R.id.signUp);
+
+      // final AppDatabase db = AppDatabase.getDatabase(this);
+       AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+               AppDatabase.class, "database-name").build();
 
        signup.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -37,55 +50,81 @@ public class Selection extends AppCompatActivity{
 
           }
 
+ private void regButtonClicked() {
+
+     final String fn = f_name.getText().toString().trim();
+     final String ln = l_name.getText().toString().trim();
+     final String rm = reg_email.getText().toString().trim();
+     final String rp = reg_password.getText().toString().trim();
+
+     if (
+             TextUtils.isEmpty(f_name.getText().toString().trim()) &&
+                     TextUtils.isEmpty(l_name.getText().toString().trim()) &&
+                     TextUtils.isEmpty(reg_email.getText().toString().trim()) &&
+                     TextUtils.isEmpty(reg_password.getText().toString().trim())
+     ) {
+         f_name.setError("required");
+         l_name.setError("required");
+         reg_email.setError("required");
+         reg_password.setError("required");
+         return;
+
+     } else {
+         //what should be done if all parameters are correct
+
+         //db.AppDatabase.insertAll( );
+
+         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                 AppDatabase.class, "persons").allowMainThreadQueries().build();
+
+         PersonInfoDao personInfoDao = db.getPersonInfoDao();
+
+         PersonInfo personInfo = new PersonInfo();
+         personInfo.setFname(fn);
+         personInfo.setLname(ln);
+         personInfo.setEmail(rm);
+         personInfo.setPassword(rp);
+
+         personInfoDao.insert(personInfo);
 
 
 
 
 
+     }
 
-private void regButtonClicked(){
-    if (TextUtils.isEmpty(fname.getText().toString().trim())){
-        fname.setError("Your message");
-        return;
-
-    }else{
-        //what should be done if all parameters are correct
-        //connection to presenter
     }
+
+    private void signupButtonClicked () {
+        {
+              //EditText f_name,l_name,reg_email,reg_password;
+
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.register, null);
+            dialog.setView(dialogView);
+
+            f_name = dialogView.findViewById(R.id.reg_firstName);
+            l_name = dialogView.findViewById(R.id.reg_lastName);
+            reg_email = dialogView.findViewById(R.id.reg_email);
+            reg_password = dialogView.findViewById(R.id.reg_password);
+            reg = dialogView.findViewById(R.id.reg_register);
+
+            dialog.show();
+
+            reg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                  regButtonClicked();
+
+                }
+            });
+
+
+        }
+
+
+    }
+
 
 }
-private void signupButtonClicked(){
-    {
-
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.register, null);
-        dialog.setView(dialogView);
-
-
-        fname = dialogView.findViewById(R.id.reg_firstName);
-        lname = dialogView.findViewById(R.id.reg_lastName);
-        reg_email = dialogView.findViewById(R.id.reg_email);
-        reg_password = dialogView.findViewById(R.id.reg_password);
-        reg=dialogView.findViewById(R.id.reg_register);
-
-        dialog.show();
-
-        reg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                regButtonClicked();
-            }
-        });
-
-
-    }
-
-
-    }
-
-
-
-
-
-    }
